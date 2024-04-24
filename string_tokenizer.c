2652e08061ac50709cfc1b79c69d029d41319bcd
Guillom8769 <8769@holbertonstudents.com>
2024-04-24 13:18:35 +0200

    add function string_tokenizer
    Co-authored-by: khadmalek <khadmalek@users.noreply.github.com>

---
+++ string_tokenizer.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+* tokenize - Divide a string into tokens using delimiters
+* @input: The string to tokenize
+* Return: A pointer to an array of strings
+*/
+char **tokenize(char *input)
+{
+	char *token;
+	char **args = NULL;
+	int argCount = 0;
+
+	/* Allocate memory for arguments */
+	args = malloc(sizeof(char *));
+	if (args == NULL)
+	{
+		perror("Memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(input, " \t\n"); /* Tokenize input using delimiters*/
+
+	while (token != NULL)
+	{
+		args[argCount] = strdup(token); /* Copy each token to args */
+		if (args[argCount] == NULL)
+		{
+			perror("Memory allocation failed");
+			exit(EXIT_FAILURE);
+		}
+		argCount++;
+		token = strtok(NULL, " \t\n"); /* Get next token */
+		/*Reallocate memory for next element*/
+		args = realloc(args, (argCount + 1) * sizeof(char *));
+		if (args == NULL)
+		{
+			perror("Memory allocation failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	/* Set last element of args to NULL */
+	args[argCount] = NULL;
+	if (argCount == 0) /*Free memory if no argument find*/
+	{
+		free(args);
+		return (NULL);
+	}
+	return (args);
+}
